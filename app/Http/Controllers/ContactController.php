7b71fbd6eb5f91b7fe77494269a1f402a9800a43
YutaNizuka <niduka81@gmail.com>
2023-11-26 23:29:52 +0900

    add バリデーション

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact.index');
+    }
+
+    public function complete()
+    {
+        return view('contact.complete');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/contact',[\App\Http\Controllers\ContactController::class,'index'])->name('contact');
+Route::post('/contact',[\App\Http\Controllers\ContactController::class,'sendmail']);
+Route::get('/contact/complete',[\App\Http\Controllers\ContactController::class,'complete'])->name('contact.complete');
