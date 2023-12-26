a41d7c7e1e8b378964decd21e0d5c29981a96762
YutaNizuka <niduka81@gmail.com>
2023-12-27 00:15:43 +0900

    add lang file

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+
 
 class ContactController extends Controller
 {
@@ -12,14 +13,9 @@ class ContactController extends Controller
         return view('contact.index');
     }
 
-    function sendMail(Request $request) {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'name_kana' => ['required', 'string', 'max:255', 'regex:/^[ァ-ロワンヴー]*$/u'],
-            'phone' => ['nullable', 'regex:/^0(\d-?\d{4}|\d{2}-?\d{3}|\d{3}-?\d{2}|\d{4}-?\d|\d0-?\d{4})-?\d{4}$/'],
-            'email' => ['required', 'email'],
-            'body' => ['required', 'string', 'max:2000'],
-        ]);
+    function sendMail(ContactRequest $request) {
+        $validated = $request->validate(
+);
 
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)
